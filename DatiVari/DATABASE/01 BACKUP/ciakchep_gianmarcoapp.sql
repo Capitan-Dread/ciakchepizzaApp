89cf2484a12615422d48887cd124722ba99aac55
black-jack <black-jack@Black-Jack.Black-Jack>
2021-03-11 03:43:04 +0100

    DATABASE AGGIORNATO : ordiniPremi TABELLA PER DE GRASSI 10/03/2021

--- DatiVari/DATABASE/01 BACKUP/ciakchep_gianmarcoapp.sql
+++ DatiVari/DATABASE/01 BACKUP/ciakchep_gianmarcoapp.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Creato il: Feb 18, 2021 alle 00:53
+-- Creato il: Mar 10, 2021 alle 05:03
 -- Versione del server: 10.3.27-MariaDB-cll-lve
--- Versione PHP: 7.3.6
+-- Versione PHP: 7.3.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -40,7 +40,8 @@ CREATE TABLE `classiche` (
 --
 
 INSERT INTO `classiche` (`id`, `nome`, `descrizione`, `prezzo`) VALUES
-(1, 'Rossa', 'Pomodoro, Olio e Origano', 3.000000000);
+(1, 'Rossa', 'Pomodoro, Olio e Origano', 3.000000000),
+(2, 'Margherita', 'pommozz', 2.500000000);
 
 -- --------------------------------------------------------
 
@@ -66,11 +67,7 @@ CREATE TABLE `clienti` (
 --
 
 INSERT INTO `clienti` (`id`, `cap`, `nome`, `cognome`, `indirizzo`, `data`, `tel`, `email`, `psw`, `score`) VALUES
-(99352, 00013, 'Gianmarco ', 'De Grassi', 'Via IV Novembre, 36', '29/03/2000', '3345354270', 'gianmarcodegrassi29@hotmail.com', 'a9245fd7fbeeef9342ceed98a1e4875ed26044f9da43a59be252531b3b3c4564479d28058e05751615c8617023c00f5cf3fe44a495da395149d06be7e0c73901', NULL),
-(99353, 00123, 'abc', 'ABC', 'VVVVVVVVVVVVVV', '2/6/6', '34567890', 'ciao@ciao.it', '5cbedc3ca4ccffc86cb05c01fe5d3d0263b398293cc782452044477f7f16b28b1819c47f9db429a9f40b54872853defca3f589a66de93d65b7503df8a257d6ef', NULL),
-(99406, 01234, 'sjnfjkvn', 'nvfjlqnfvkjfqò', 'dndnffj', '4/8/8', '5432', 'prova@gmail.com', '314c915555bf35b326a55805a052a5a30808fdfc45aae3e0c40abcf7546b6d17abd635d59ce3e47d4472a48e221301f22ee9dca85f2b56665f3bb62119649a4a', NULL),
-(99413, 01234, 'Ciaone', 'Bellone', 'Via del ciaobellone,27', '16/01/1999', '34354334', 'ciaone@ciaone.it', '5cbedc3ca4ccffc86cb05c01fe5d3d0263b398293cc782452044477f7f16b28b1819c47f9db429a9f40b54872853defca3f589a66de93d65b7503df8a257d6ef', NULL),
-(99414, 00034, 'fg', 'ggt', 'hgfg', '3566', '655', 'ggg', '22f91e1ec976c25882a3aa2dfc033f1ee4188b50061142389390ecbe9ce3e56208ad087a9de6296785a1021c1a0efbaa0eb8102d046d5df4c961284dc95593d7', NULL);
+(99421, 01234, 'hello', 'world', 'asdfghjk', '1/8/8', '123456789', 'hello@hello.it', '2791885b0d925584ec17996fa052736efd6464455686f3575cff78256666f622b63afc5b0a5dd061ed38906e40660dd4f339624ad29e2b16d0711cd4db0f08cd', 0);
 
 -- --------------------------------------------------------
 
@@ -140,6 +137,23 @@ INSERT INTO `ordini` (`id`, `cap`, `descrizione`, `buono`, `data`, `prezzo`, `em
 -- --------------------------------------------------------
 
 --
+-- Struttura della tabella `ordiniPremi`
+--
+
+CREATE TABLE `ordiniPremi` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(500) DEFAULT NULL,
+  `punti` int(255) DEFAULT NULL,
+  `idClt` int(255) DEFAULT NULL,
+  `modRitiro` varchar(500) DEFAULT NULL,
+  `quantita` int(255) DEFAULT NULL,
+  `cap` int(5) DEFAULT NULL,
+  `descrizione` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Struttura della tabella `pizzerie`
 --
 
@@ -174,6 +188,14 @@ CREATE TABLE `titolari` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dump dei dati per la tabella `titolari`
+--
+
+INSERT INTO `titolari` (`id`, `cap`, `nome`, `cognome`, `indirizzo`, `data`, `cf`, `tel`, `email`, `psw`, `id_pizzeria`, `auth`) VALUES
+(1, 00013, 'Fabio', 'Sociani', '', '', NULL, '', 'ciakchepizzapp@gmail.com', 'eef93c72a9e0540c4e949752acec38e8c68f12ddf5d6c333014ce1b72f7751538008c97899bd349680cea4ff16ab32faab8d8d48a8b068e313326c5a845d1815', NULL, 'Si'),
+(10100101, 01234, 'Mario', 'Rossi', '', '', NULL, '', 'ad', '22f61a6aafc0846298ed5b0a58808b411d83102a407409d85e7ce2187791a702c30985d663f01b98caa848a669393e8e0c37a422190099e3a14635b90218264f', NULL, 'No');
+
+--
 -- Indici per le tabelle scaricate
 --
 
@@ -203,6 +225,12 @@ ALTER TABLE `game`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indici per le tabelle `ordiniPremi`
+--
+ALTER TABLE `ordiniPremi`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indici per le tabelle `pizzerie`
 --
 ALTER TABLE `pizzerie`
@@ -224,18 +252,24 @@ ALTER TABLE `titolari`
 -- AUTO_INCREMENT per la tabella `classiche`
 --
 ALTER TABLE `classiche`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `clienti`
 --
 ALTER TABLE `clienti`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99415;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99422;
 
 --
 -- AUTO_INCREMENT per la tabella `creaPizza`
 --
 ALTER TABLE `creaPizza`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `ordiniPremi`
+--
+ALTER TABLE `ordiniPremi`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -248,7 +282,7 @@ ALTER TABLE `pizzerie`
 -- AUTO_INCREMENT per la tabella `titolari`
 --
 ALTER TABLE `titolari`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10100102;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
